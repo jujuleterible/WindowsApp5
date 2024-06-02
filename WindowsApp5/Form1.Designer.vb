2b628c8e50e8969ce9b83d9c3b0122971df08642
jujuleterible <165694484+jujuleterible@users.noreply.github.com>
2024-06-02 17:55:48 +0200

    GGGGJJ

--- WindowsApp5/Form1.Designer.vb
+++ WindowsApp5/Form1.Designer.vb
@@ -38,7 +38,7 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(542, 60)
+        Me.Button2.Location = New System.Drawing.Point(414, 78)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(184, 74)
         Me.Button2.TabIndex = 1
